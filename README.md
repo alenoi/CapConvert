b7ddb6d12987a91a35e6f09d83f5db7f49c961ab
alenoi <ptg.tomi@gmail.com>
2022-08-11 17:05:45 +0200

    HEVC->MP4 implementation

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # HEIC to JPG converter Discord bot
-This is a small application for converting HEIC files to JPG on the fly when someone posts them on a channel. The bot will automatically reply with the converted images to the original message
+This is a small application for converting HEIC files to JPG and HEVC files to MP4 on the fly when someone posts them on a channel. The bot will automatically reply with the converted images to the original message
 
 It can run locally with python interpreter or with the exe int the dist folder. 
 You can type in your TOKEN and serverID at every start or you can set them up in the .env file. To use the file please rename it to ".env"
--- h2j_bot.py
+++ h2j_bot.py
@@ -1,10 +1,11 @@
 import os
-
+import re
 import discord
 import requests
 from PIL import Image
 from dotenv import load_dotenv
 from pillow_heif import register_heif_opener
+import moviepy.editor as moviepy
 
 client = discord.Client()
 register_heif_opener()
@@ -28,42 +29,45 @@ async def on_message(self, message: discord.Message) -> None:
 
 @client.event
 async def on_ready():
-    i = 0
+
     for guild in client.guilds:
-        print(f'{client.user.name} has connected to {client.guilds[i].name}!')
-        i+=1
+        print(f'{client.user.name} has connected to {guild.name}!')
+
 
 
 @client.event
 async def on_message(message):
     files_to_delete: list[str] = []
     files_to_send: list[discord.File] = []
-    isheic = False
+    isconvert = False
     if not message.author.bot and len(message.attachments) > 0:
         for item in message.attachments:
-            urlparts_dot = item.url.split('.')
-            if urlparts_dot[len(urlparts_dot) - 1].lower() == "heic":
-                isheic = True
-                await save_and_convert(files_to_send, files_to_delete, item)
-        if isheic:
-            await send_images(files_to_send, message)
+            ext = item.url.split('.')[len(item.url.split('.')) - 1].lower()
+            if re.search('heic',ext):
+                isconvert = True
+                await heic2jpg(files_to_send, files_to_delete, item)
+            elif re.search('hevc',ext) or re.search('mp4',ext):
+                isconvert = True
+                await hevc2mp4(files_to_send, files_to_delete, item)
+        if isconvert:
+            await send_files(files_to_send, message)
             for item in files_to_delete:
                 os.remove(item)
 
 
-async def send_images(files_to_send, message):
+async def send_files(files_to_send, message):
     CHANNEL = client.get_channel(message.channel.id)
     if message.author.nick:
-        mess = message.author.nick + " ezt nem bírta rendesen elküldeni:"
+        mess = f'Let me convert for you the files {message.author.nick} was unable to'
     else:
-        mess = message.author.name + " ezt nem bírta rendesen elküldeni:"
+        mess = f'Let me convert for you the files {message.author.name} was unable to'
     await CHANNEL.send(files=files_to_send,
                        content=mess,
                        # the message you want to appear next to the images
                        reference=message)
 
 
-async def save_and_convert(files_to_send, files_to_delete, item):
+async def heic2jpg(files_to_send, files_to_delete, item):
     urlparts_slash = item.url.split('/')
     ogfilename = urlparts_slash[len(urlparts_slash) - 1]
     open(ogfilename, "wb").write(requests.get(item.url).content)
@@ -72,7 +76,19 @@ async def save_and_convert(files_to_send, files_to_delete, item):
     image.save(filename, "jpeg")
     files_to_send.append(discord.File(filename))
     files_to_delete.append(filename)
-    os.remove(ogfilename)
+    files_to_delete.append(ogfilename)
+
+
+async def hevc2mp4(files_to_send, files_to_delete, item):
+    urlparts_slash = item.url.split('/')
+    ogfilename = urlparts_slash[len(urlparts_slash) - 1]
+    open(ogfilename, "wb").write(requests.get(item.url).content)
+    filename = f'{ogfilename.split(".")[0]}_c.mp4'
+    clip = moviepy.VideoFileClip(ogfilename)
+    clip.write_videofile(f'{filename}')
+    files_to_send.append(discord.File(filename))
+    files_to_delete.append(filename)
+    files_to_delete.append(ogfilename)
 
 
 if TOKEN != "":
