bbf5ebb844eb4dcb6bd274681ca2e0f27b4bf951
alenoi <ptg.tomi@gmail.com>
2022-08-26 12:16:14 +0200

    optimized unnecesary tiktok video resize

--- capconvert.py
+++ capconvert.py
@@ -13,7 +13,6 @@ intents = discord.Intents(messages=True, message_content=True, guilds=True)
 intents.typing = True
 intents.reactions = True
 
-
 client = discord.Client(intents=intents)
 register_heif_opener()
 
@@ -54,16 +53,17 @@ async def on_message(message):
             await tiktok_download(files_to_send, files_to_delete, message)
         if len(message.attachments) > 0:
             for item in message.attachments:
-                ext = item.url.split('.')[len(item.url.split('.')) - 1].lower()
-                if re.search('heic', ext):
+                # ext = item.url.split('.')[len(item.url.split('.')) - 1].lower()
+                if 'heic' in item.url.lower():
                     await heic2jpg(files_to_send, files_to_delete, item)
-                elif re.search('hevc', ext) or re.search('mp4', ext):
+                elif 'hevc' in item.url.lower() or 'mp4' in item.url.lower():
                     await hevc2mp4(files_to_send, files_to_delete, item)
+
     if len(files_to_send) > 0:
         try:
             await send_files(files_to_send, message)
-        except:
-            print("Could not send the file")
+        except Exception as e:
+            print(e)
     if len(files_to_delete) > 0:
         for item in files_to_delete:
             os.remove(item)
@@ -71,29 +71,34 @@ async def on_message(message):
 
 async def tiktok_download(files_to_send, files_to_delete, message):
     url = message.content.split('/')[3]
-    ogfilename = f'{url}.mp4'
-    filename = f'{ogfilename}_c.mp4'
+    ogfilename = f'{url}.webm'
+    filename = f'{url}_c.mp4'
     os.system(f'python3.10 -m tiktok_downloader --url https://vt.tiktok.com/{url} --tiktok --save {ogfilename}')
     clip = moviepy.VideoFileClip(ogfilename)
     clipsize = 8000000 / os.path.getsize(ogfilename)
     if clipsize <= 1:
-        clip = clip.resize(clipsize-0.02)
-    clip.write_videofile(f'{filename}', threads=4)
-    files_to_send.append(discord.File(filename))
-    files_to_delete.append(filename)
+        clip = clip.resize(clipsize - 0.01)
+        clip.write_videofile(f'{filename}', threads=4)
+        files_to_send.append(discord.File(f'{filename}'))
+        files_to_delete.append(filename)
+    else:
+        files_to_send.append(discord.File(ogfilename))
     files_to_delete.append(ogfilename)
 
 
 async def send_files(files_to_send, message):
     CHANNEL = client.get_channel(message.channel.id)
     if message.author.nick:
-        mess = f'Let me convert for you the files {message.author.nick} was unable to'
+        embedVar = discord.Embed(title=f"", description=f'{message.content}', color=RGBtoColor('FF8800'))
     else:
-        mess = f'Let me convert for you the files {message.author.name} was unable to'
-    await CHANNEL.send(files=files_to_send,
-                       content=mess,
-                       # the message you want to appear next to the images
-                       reference=message)
+        embedVar = discord.Embed(title=f"", description=f'{message.content}', color=RGBtoColor('FF8800'))
+    embedVar.set_author(name=message.author.name, icon_url=message.author.avatar)
+    await CHANNEL.send(embed=embedVar,
+                       files=files_to_send,
+                       # content=mess,
+                       reference=message
+                       )
+    await message.delete()
 
 
 async def heic2jpg(files_to_send, files_to_delete, item):
@@ -130,6 +135,14 @@ async def hevc_check(ogfilename):
         if track.track_type == "Video":
             codec = "{t.format}".format(t=track)
     return codec
+
+
+def RGBtoColor(xrgb):
+    if len(xrgb) < 6:
+        xrgb = xrgb + "000000"
+    return discord.Color.from_rgb(int(xrgb[0:2], 16),
+                                  int(xrgb[2:4], 16),
+                                  int(xrgb[4:6], 16))
 
 
 if TOKEN != "":
