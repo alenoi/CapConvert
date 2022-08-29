91eb39eaf941044dc410b161739c550dc3f79db1
alenoi <ptg.tomi@gmail.com>
2022-08-29 12:27:05 +0200

    refactor

--- capconvert.py
+++ capconvert.py
@@ -1,5 +1,6 @@
 import os
 import re
+import sys
 
 import discord
 import moviepy.editor as moviepy
@@ -17,7 +18,7 @@ client = discord.Client(intents=intents)
 register_heif_opener()
 
 TOKEN = ""
-SERVER = ""
+fileSizeLimit = 8000000
 
 if os.path.isfile(".env"):
     load_dotenv()
@@ -25,7 +26,7 @@ if os.path.isfile(".env"):
 else:
     print('Cannot find environment file\n')
     TOKEN = input('Please enter your bot TOKEN!\n')
-    saveToken = input('Do you want to save this TOKEN?\n y/n')
+    saveToken = input('Do you want to save this TOKEN?\n [Y/N] \n')
     if saveToken.upper() == 'Y':
         f = open(".env", "w")
         f.write(f'DISCORD_TOKEN={TOKEN}')
@@ -46,88 +47,127 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    files_to_delete: list[str] = []
-    files_to_send: list[discord.File] = []
     if not message.author.bot:
-        if 'tiktok.com/' in message.content:
-            await tiktok_download(files_to_send, files_to_delete, message)
-        if len(message.attachments) > 0:
-            for item in message.attachments:
-                # ext = item.url.split('.')[len(item.url.split('.')) - 1].lower()
-                if 'heic' in item.url.lower():
-                    await heic2jpg(files_to_send, files_to_delete, item)
-                elif 'hevc' in item.url.lower() or 'mp4' in item.url.lower():
-                    await hevc2mp4(files_to_send, files_to_delete, item)
+        if 'tiktok.com/' in message.content or len(message.attachments) > 0:
+            await media_process(message)
 
+
+class mediaFile:
+    url: str
+    fileName: str
+    convertedFile: str
+    type: str
+
+
+async def media_process(ogmsg):
+    mediafiles: list[mediaFile] = []
+    if 'tiktok.com/' in ogmsg.content:
+        mediafiles.append(urlParse(ogmsg.content))
+    if len(ogmsg.attachments) > 0:
+        for item in ogmsg.attachments:
+            mediafiles.append(urlParse(item.url))
+
+    embedmsg = await send_embed(ogmsg, mediafiles)
+    await media_download(mediafiles)
+    await media_convert(mediafiles)
+    await send_files(mediafiles, embedmsg)
+    cleanup(mediafiles)
+
+
+async def send_embed(ogmsg, ogFiles):
+    CHANNEL = ogmsg.channel
+    if ogmsg.author.nick:
+        embedVar = discord.Embed(title='Message:', description=ogmsg.content, color=0xFF8800)
+    else:
+        embedVar = discord.Embed(title='Message:', description=ogmsg.content, color=0xFF8800)
+    embedVar.set_author(name=ogmsg.author.name, icon_url=ogmsg.author.avatar)
+    embedVar.set_footer(text="File processing is in progress",
+                        icon_url="https://cdn.discordapp.com/avatars/1006542721990807592/b377f09c6a20f90d9d8378e07fa13682.png?size=1024")
+    fileList = ""
+    for file in ogFiles:
+        fileList += f'\n{file.fileName}'
+    embedVar.add_field(name='Attachments:', value=fileList, inline=False)
+    message = await CHANNEL.send(embed=embedVar, files=None, reference=ogmsg)
+    await ogmsg.delete()
+    return message
+
+
+def urlParse(url):
+    mediafile = mediaFile()
+    mediafile.url = url
+    urlparts_slash = url.split('/')
+    mediafile.fileName = urlparts_slash[len(urlparts_slash) - 1].lower()
+    if mediafile.fileName == "":
+        mediafile.fileName = urlparts_slash[len(urlparts_slash) - 2].lower()
+    if "tiktok" in url:
+        mediafile.type = "tiktok"
+        mediafile.fileName += ".webm"
+    if "discordapp" in url:
+        mediafile.type = "discord"
+    return mediafile
+
+
+async def media_download(mediafiles: list[mediaFile]):
+    for item in mediafiles:
+        if item.type == "tiktok":
+            os.system(f'python3.10 -m tiktok_downloader --url {item.url} --tiktok --save {item.fileName}')
+        if item.type == "discord":
+            open(item.fileName, "wb").write(requests.get(item.url).content)
+
+
+async def send_files(mediafiles: list[mediaFile], message):
+    files_to_send: list[discord.File] = []
+    embedVar = message.embeds[0]
+    for item in mediafiles:
+        files_to_send.append(discord.File(item.convertedFile))
+    embedVar.set_footer(text="", icon_url="")
     if len(files_to_send) > 0:
-        try:
-            await send_files(files_to_send, message)
-        except Exception as e:
-            print(e)
-    if len(files_to_delete) > 0:
-        for item in files_to_delete:
-            os.remove(item)
+        await message.channel.send(embed=embedVar, files=files_to_send, reference=message)
+        await message.delete()
 
 
-async def tiktok_download(files_to_send, files_to_delete, message):
-    url = message.content.split('/')[3]
-    ogfilename = f'{url}.webm'
-    filename = f'{url}_c.webm'
-    os.system(f'python3.10 -m tiktok_downloader --url https://vt.tiktok.com/{url} --tiktok --save {ogfilename}')
-    clip = moviepy.VideoFileClip(ogfilename)
-    clipsize = 8000000 / os.path.getsize(ogfilename)
-    if clipsize <= 1:
-        # clip = clip.resize(clipsize - 0.01)
-        clip.write_videofile(f'{filename}', threads=4, codec='libvpx')
-        files_to_send.append(discord.File(f'{filename}'))
-        files_to_delete.append(filename)
+def cleanup(mediafiles: list[mediaFile]):
+    for item in mediafiles:
+        if os.path.isfile(item.fileName):
+            os.remove(item.fileName)
+        if os.path.isfile(item.convertedFile):
+            os.remove(item.convertedFile)
+    mediafiles.clear()
+
+
+async def media_convert(mediafiles: list[mediaFile]):
+    for item in mediafiles:
+        if 'heic' in item.fileName:
+            item.convertedFile = await imageConvert(item.fileName)
+        if 'hevc' in item.fileName or 'mp4' in item.fileName or 'webm' in item.fileName:
+            item.convertedFile = await videoConvert(item.fileName)
+
+
+async def imageConvert(path):
+    cpath = f'{path.split(".")[0]}_c.jpg'
+    image = Image.open(path)
+    image.save(cpath, "jpeg")
+    return cpath
+
+
+async def videoConvert(path):
+    cpath = f'{path.split(".")[0]}_c.webm'
+    clip = moviepy.VideoFileClip(path)
+    codec = hevc_check(path).lower()
+    clipsize = fileSizeLimit / os.path.getsize(path) - 0.01
+    if codec == 'hevc' or codec == 'av1' or clipsize <= 1:
+        clip.write_videofile(cpath, threads=4, codec='libvpx')
+        clipsize = fileSizeLimit / os.path.getsize(cpath) - 0.01
+        if clipsize <= 1:
+            clip.resize(clipsize)
+            clip.write_videofile(cpath, threads=4, codec='libvpx')
     else:
-        files_to_send.append(discord.File(ogfilename))
-    files_to_delete.append(ogfilename)
+        clip.close()
+        cpath = path
+    return cpath
 
 
-async def send_files(files_to_send, message):
-    CHANNEL = client.get_channel(message.channel.id)
-    if message.author.nick:
-        embedVar = discord.Embed(title=f"", description=f'{message.content}', color=RGBtoColor('FF8800'))
-    else:
-        embedVar = discord.Embed(title=f"", description=f'{message.content}', color=RGBtoColor('FF8800'))
-    embedVar.set_author(name=message.author.name, icon_url=message.author.avatar)
-    await CHANNEL.send(embed=embedVar,
-                       files=files_to_send,
-                       reference=message
-                       )
-    await message.delete()
-
-
-async def heic2jpg(files_to_send, files_to_delete, item):
-    urlparts_slash = item.url.split('/')
-    ogfilename = urlparts_slash[len(urlparts_slash) - 1]
-    open(ogfilename, "wb").write(requests.get(item.url).content)
-    filename = ogfilename[:len(ogfilename) - 5] + ".jpg"
-    image = Image.open(ogfilename)
-    image.save(filename, "jpeg")
-    files_to_send.append(discord.File(filename))
-    files_to_delete.append(filename)
-    files_to_delete.append(ogfilename)
-
-
-async def hevc2mp4(files_to_send, files_to_delete, item):
-    urlparts_slash = item.url.split('/')
-    ogfilename = urlparts_slash[len(urlparts_slash) - 1]
-    open(ogfilename, "wb").write(requests.get(item.url).content)
-    codec = await hevc_check(ogfilename)
-    print(codec.upper())
-    if codec.upper() == 'HEVC' or codec.upper() == 'AV1':
-        filename = f'{ogfilename.split(".")[0]}_c.mp4'
-        clip = moviepy.VideoFileClip(ogfilename)
-        clip.write_videofile(f'{filename}')
-        files_to_send.append(discord.File(filename))
-        files_to_delete.append(filename)
-        files_to_delete.append(ogfilename)
-
-
-async def hevc_check(ogfilename):
+def hevc_check(ogfilename):
     info = MediaInfo.parse(ogfilename)
     codec = ""
     for track in info.tracks:
@@ -136,18 +176,8 @@ async def hevc_check(ogfilename):
     return codec
 
 
-def RGBtoColor(xrgb):
-    if len(xrgb) < 6:
-        xrgb = xrgb + "000000"
-    return discord.Color.from_rgb(int(xrgb[0:2], 16),
-                                  int(xrgb[2:4], 16),
-                                  int(xrgb[4:6], 16))
-
-
-if TOKEN != "":
-    try:
-        client.run(TOKEN)
-    except:
-        print('Error: The connection to the server cannot be established, please check your serverID and Token!')
-else:
-    print('Error: There is no valid serverID or Token!')
+try:
+    print("CapConvert initialization")
+    client.run(TOKEN)
+except:
+    print('Error: The connection to the server cannot be established, please check your Token!')
